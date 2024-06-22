9f337dc8b0e5dda736fa1c1719a8e7648d74fec5
Michael Gruben <michael.gruben@gmx.net>
2024-06-22 15:33:23 +0200

    further dialogue

--- view/src/LoggedIn.tsx
+++ view/src/LoggedIn.tsx
@@ -3,9 +3,23 @@ import { CarrotData } from './types';
 
 const NewRecipeDialog = ({ showNewRecipeDialog, setShowNewRecipeDialog }: any) => (
   <div className='w-screen h-screen bg-gray-dark bg-opacity-90 absolute'>
-    <div className='blur-none'>
-      Hier kannst du ein neues Rezept anlegen
-      <button onClick={() => setShowNewRecipeDialog(!showNewRecipeDialog)}>Schließen</button>
+    <div className='w-fit m-auto mt-4 bg-primary flex flex-col'>
+      <div className='flex flex-row flex-nowrap'>
+        <span>Hier kannst du ein neues Rezept anlegen</span>
+        <button onClick={() => setShowNewRecipeDialog(!showNewRecipeDialog)}>Schließen</button>
+      </div>
+      <form action="/" method="post" className='flex flex-col'>
+        <label htmlFor='title'>Rezepttitel</label>
+        <input type="text" id="title" />
+        Aufwand
+        <label htmlFor='effort0'>gering</label>
+        <input type="radio" value="0" name="effort" id="effort0" />
+        <label htmlFor='effort0'>mittel</label>
+        <input type="radio" value="1" name="effort" id="effort1" />
+        <label htmlFor='effort0'>hoch</label>
+        <input type="radio" value="2" name="effort" id="effort2" />
+        <input type="submit" value="Speichern" />
+      </form>
     </div>
   </div>);
 
--- view/src/components/ui/LoginForm.tsx
+++ view/src/components/ui/LoginForm.tsx
@@ -11,10 +11,10 @@ export const LoginForm = () => {
         register,
     } = useForm<Inputs>();
     return <form action="/" method="POST">
-        <label>Login</label>
-        <input className="form-input" {...register("login", { required: true })} />
-        <label>Password</label>
-        <input className="form-input" type="password" {...register("password", { required: true })} />
+        <label htmlFor="login">Login</label>
+        <input id="login" className="form-input" {...register("login", { required: true })} />
+        <label htmlFor="password">Password</label>
+        <input id="password" className="form-input" type="password" {...register("password", { required: true })} />
         <input type="submit" value="Anmelden" />
     </form>;
 };
